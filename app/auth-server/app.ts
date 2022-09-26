0777c31bcd1870399258370d38906fc90f458fb6
mp281x <paludgnachmatteo.dev@gmail.com>
2022-09-26 15:58:10 +0200

    test auth server

---
+++ app/auth-server/app.ts
@@ -0,0 +1,62 @@
+import express, { Request, Response, NextFunction } from 'express';
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+app.use(cookieParser());
+app.listen(3000, () => console.log(`Auth server running`));
+
+async function getGithubUser({ code }: { code: string }) {
+	const githubToken = await axios
+		.post(`https://github.com/login/oauth/access_token?client_id=${process.env.client_id}&client_secret=${process.env.client_secret}&code=${code}`)
+		.then((res) => res.data)
+		.catch((error) => {
+			throw error;
+		});
+
+	const accessToken = new URLSearchParams(githubToken).get('access_token');
+	const userData = await axios
+		.get('https://api.github.com/user', { headers: { Authorization: `Bearer ${accessToken}` } })
+		.then((res) => res.data)
+		.catch((error) => {
+			throw error;
+		});
+
+	return { username: userData.login };
+}
+
+app.get('/auth', async (req: Request, res: Response) => {
+	try {
+		const token = req.cookies['github-jwt'];
+
+		if (token === undefined || token === '') {
+			const code = req.query.code as string;
+			if (code === undefined || code === '') {
+				const redirect = `https://auth.dev.mp281x.xyz/auth?scope=user:email`;
+				return res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.client_id}&redirect_uri=${redirect}`);
+			}
+
+			const githubUser = await getGithubUser({ code });
+			if (githubUser.username !== 'MP281X') return res.json({ error: 'unauthorized' });
+
+			const token = jwt.sign(githubUser, process.env.jwtKey ?? '');
+			res.cookie('github-jwt', token, {
+				httpOnly: true,
+				domain: 'localhost',
+				secure: false
+			});
+
+			return res.json({ res: 'authorized' });
+		} else {
+			const tokenData = jwt.verify(token, process.env.jwtKey ?? '') as { username: string };
+			if (tokenData.username !== 'MP281X') return res.json({ error: 'unauthorized' });
+			return res.json({ res: 'authorized' });
+		}
+	} catch (error) {
+		res.status(500);
+		return res.json({ res: 'unauthorized' });
+	}
+});
---
+++ app/auth-server/auth-server.Dockerfile
@@ -0,0 +1,20 @@
+FROM node:16-alpine AS build
+WORKDIR /app
+COPY . .
+
+# build and install dependency
+RUN yarn install && npx tsc \
+    && rm -r ./node_modules \
+    && yarn install --production 
+
+# build the final image
+FROM node:16-alpine
+WORKDIR /app
+
+# copy the build and the package.json
+COPY --from=build /app/package.json /app/package.json
+COPY --from=build /app/node_modules /app/node_modules
+COPY --from=build /app/build /app/build
+
+ENV port 3000
+CMD [ "node", "build/app.js" ]
