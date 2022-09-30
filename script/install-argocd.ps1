efd881b57cd24eeb2a5811a8ac658208e3a27ee1
mp281x <paludgnachmatteo.dev@gmail.com>
2022-09-30 22:07:44 +0200

    test configmap

--- script/install-argocd.ps1
+++ script/install-argocd.ps1
@@ -1,2 +1,4 @@
 kubectl create namespace argocd;
+kubectl apply -f clusters/key/github-registry.yaml;
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/stable/manifests/install.yaml
