8845b1d7b73751fe21a0be2469d95f9c2c02fd0a
mp281x <paludgnachmatteo.dev@gmail.com>
2022-12-26 17:28:26 +0100

    reenabled velero

--- script/velero.ps1
+++ script/velero.ps1
@@ -1,1 +1,7 @@
-velero install --provider aws --use-restic --plugins velero/velero-plugin-for-aws:v1.5.0 --bucket backup --secret-file ../key/velero.secret --backup-location-config region=eu,s3ForcePathStyle="true",s3Url=https://eu2.contabostorage.com
+velero install `
+    --provider aws `
+    --use-restic `
+    --plugins velero/velero-plugin-for-aws:v1.5.0 `
+    --secret-file ./key/velero.secret `
+    --bucket backup `
+    --backup-location-config region=eu,s3ForcePathStyle="true",s3Url=https://eu2.contabostorage.com --dry-run -o yaml > velero.yaml
