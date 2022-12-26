7a0a3444aad3fdba3f6ea35c47f1fe37eeb05b6f
mp281x <paludgnachmatteo.dev@gmail.com>
2022-12-26 17:59:24 +0100

    test kasten

--- script/velero.ps1
+++
@@ -1,7 +0,0 @@
-velero install `
-    --provider aws `
-    --use-restic `
-    --plugins velero/velero-plugin-for-aws:v1.5.0 `
-    --secret-file ./key/velero.secret `
-    --bucket backup `
-    --backup-location-config region=eu,s3ForcePathStyle="true",s3Url=https://eu2.contabostorage.com --dry-run -o yaml > velero.yaml
