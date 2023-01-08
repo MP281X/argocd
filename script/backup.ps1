2085abb9fb5fd1ef9678de0245899bb488d048b9
mp281x <paludgnachmatteo.dev@gmail.com>
2023-01-08 23:23:44 +0100

    disabled vaultwarden

--- script/backup.ps1
+++ script/backup.ps1
@@ -1,3 +1,3 @@
 ssh mp281x@dev.mp281x.xyz 'sudo tar -zcf backup.tar.gz storage'
-scp mp281x@dev.mp281x.xyz:backup.tar.gz backup/backup.tar.gz
+scp mp281x@dev.mp281x.xyz:backup.tar.gz backup.tar.gz
 ssh mp281x@dev.mp281x.xyz 'sudo rm backup.tar.gz'
--- script/restore.ps1
+++ script/restore.ps1
@@ -1,2 +1,2 @@
-scp backup/backup.tar.gz mp281x@dev.mp281x.xyz:/home/mp281x/backup.tar.gz
-ssh mp281x@dev.mp281x.xyz 'sudo rm -r /home/mp281x/storage/* && sudo tar -xf backup.tar.gz && sudo rm backup.tar.gz'
+scp backup.tar.gz mp281x@dev.mp281x.xyz:/home/mp281x/backup.tar.gz
+ssh mp281x@dev.mp281x.xyz 'sudo rm -rf /home/mp281x/storage/* && sudo tar -xf backup.tar.gz && sudo rm backup.tar.gz'
