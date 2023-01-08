3c8c71a5429d3d63434e8181a61e5af32145b916
mp281x <paludgnachmatteo.dev@gmail.com>
2023-01-08 23:16:04 +0100

    disabled dicantieri, fix backup and restore script

--- script/backup.ps1
+++ script/backup.ps1
@@ -1,3 +1,3 @@
-ssh mp281x@dev.mp281x.xyz 'sudo tar -zcvf /home/mp281x/backup-$(date +"%d-%m-%Y").tar.gz /home/mp281x/storage'
-scp mp281x@dev.mp281x.xyz:/home/mp281x/backup-$(Get-Date -Format "dd-MM-yyyy").tar.gz backup/backup-$(Get-Date -Format "dd-MM-yyyy").tar.gz
-ssh mp281x@dev.mp281x.xyz 'sudo rm /home/mp281x/backup-$(date +"%d-%m-%Y").tar.gz'
+ssh mp281x@dev.mp281x.xyz 'sudo tar -zcf backup.tar.gz storage'
+scp mp281x@dev.mp281x.xyz:backup.tar.gz backup/backup.tar.gz
+ssh mp281x@dev.mp281x.xyz 'sudo rm backup.tar.gz'
