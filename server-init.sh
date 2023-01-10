537c864f417b159f4d8f5312b1e92f71fcda5a79
mp281x <paludgnachmatteo.dev@gmail.com>
2023-01-10 18:25:39 +0100

    removed rclone from the init script

--- server-init.sh
+++ server-init.sh
@@ -40,12 +40,6 @@ echo $SSH_CONFIG > /etc/ssh/sshd_config
 echo "" > /etc/motd
 service sshd restart
 
-echo " ------- rclone ------- "
-curl https://rclone.org/install.sh | bash
-mkdir -p /root/.config/rclone/
-vi /root/.config/rclone/rclone.conf
-mkdir -p /home/mp281x/storage
-
 echo " ------- k3s ------- "
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC=" \
     --disable=traefik \
