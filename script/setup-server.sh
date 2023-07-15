f64bb412d20d1de99a5a2fde55328f83cbb35d8f
= <=>
2023-07-15 19:28:14 +0200

    restored old longhorn version

--- script/setup-server.sh
+++ script/setup-server.sh
@@ -9,9 +9,6 @@ scp secrets/helm-chart.yaml root@dev.mp281x.xyz:/
 scp secrets/registry-k3s.yaml root@dev.mp281x.xyz:/
 scp secrets/sealedSecrets.key root@dev.mp281x.xyz:/
 
-# convert the file to the unix format
-ssh root@dev.mp281x.xyz "apt install dos2unix -y && dos2unix /server-init.sh && dos2unix /helm-chart.yaml && dos2unix /registry-k3s.yaml && dos2unix /sealedSecrets.key"
-
 # run the init script
 ssh root@dev.mp281x.xyz "sh /server-init.sh"
 
